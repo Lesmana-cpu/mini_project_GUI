2d3a57ba0e7a398aa1294ab6cd1fdaa5113eb9e3
Lesmana-cpu <lesmanadarajat42@gmail.com>
2025-05-30 16:55:56 +0700

    Delete eksperiment directory

--- eksperiment/exercise.py
+++
@@ -1,6 +0,0 @@
-
-
-for i in range(7,14,2):
-    for j in range(i):
-        for k in range(j):
-            print(i, j,k)
--- eksperiment/variabels.py
+++
@@ -1,5 +0,0 @@
-while True:
-    x, y, z = (input('masukkan nama,umur,dan kelamin')).split()
-    print(x)
-    print(type(y))
-    print(z)
